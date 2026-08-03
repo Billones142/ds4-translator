2a5ea95d563e0a150277b8631073ec52c86c4a19
Billones142 <stefanomerinoderui@gmail.com>
2026-08-03 11:55:26 -0300

    Fix: spoof subsystem to 'usb' for fake parent queries in udev wrapper

--- src/udev-spoof.c
+++ src/udev-spoof.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <stdint.h>
 
 struct udev_device;
 
 static struct udev_device *(*real_get_parent)(struct udev_device *, const char *, const char *) = NULL;
 static const char *(*real_get_sysattr)(struct udev_device *, const char *) = NULL;
 static const char *(*real_get_property)(struct udev_device *, const char *) = NULL;
+static const char *(*real_get_subsystem)(struct udev_device *) = NULL;
 
 static void init_functions() {
     if (!real_get_parent) {
@@ -20,8 +22,9 @@ static void init_functions() {
         real_get_parent = dlsym(handle, "udev_device_get_parent_with_subsystem_devtype");
         real_get_sysattr = dlsym(handle, "udev_device_get_sysattr_value");
         real_get_property = dlsym(handle, "udev_device_get_property_value");
-        fprintf(stderr, "[udev-spoof] Library initialized: parent=%p, sysattr=%p, property=%p\n", 
-                real_get_parent, real_get_sysattr, real_get_property);
+        real_get_subsystem = dlsym(handle, "udev_device_get_subsystem");
+        fprintf(stderr, "[udev-spoof] Library initialized: parent=%p, sysattr=%p, property=%p, subsystem=%p\n", 
+                real_get_parent, real_get_sysattr, real_get_property, real_get_subsystem);
     }
 }
 
@@ -29,21 +32,15 @@ static int is_virtual_sony_controller(struct udev_device *dev) {
     init_functions();
     if (!dev || !real_get_property) return 0;
     
-    const char *vid = real_get_property(dev, "ID_VENDOR_ID");
-    const char *pid = real_get_property(dev, "ID_MODEL_ID");
-    const char *devpath = real_get_property(dev, "DEVPATH");
-    
-    // Debug print properties for all devices that have a VID
-    if (vid) {
-        fprintf(stderr, "[udev-spoof] Checked device: VID=%s, PID=%s, DevPath=%s\n", 
-                vid, pid ? pid : "NULL", devpath ? devpath : "NULL");
-    }
+    struct udev_device *clean_dev = (struct udev_device *)((uintptr_t)dev & ~1);
+    const char *vid = real_get_property(clean_dev, "ID_VENDOR_ID");
+    const char *pid = real_get_property(clean_dev, "ID_MODEL_ID");
+    const char *devpath = real_get_property(clean_dev, "DEVPATH");
     
     int is_virtual = devpath && strstr(devpath, "/devices/virtual/misc/uhid/") != NULL;
     
     if (is_virtual && vid && !strcmp(vid, "054c")) {
         if (pid && (!strcmp(pid, "05c4") || !strcmp(pid, "0ce6"))) {
-            fprintf(stderr, "[udev-spoof] Identified virtual Sony controller: VID=%s, PID=%s, DevPath=%s\n", vid, pid, devpath);
             return 1;
         }
     }
@@ -54,8 +51,16 @@ struct udev_device *udev_device_get_parent_with_subsystem_devtype(struct udev_de
     init_functions();
     
     if (subsystem && !strcmp(subsystem, "usb") && is_virtual_sony_controller(udev_device)) {
-        fprintf(stderr, "[udev-spoof] Spoofing parent query for virtual Sony controller\n");
-        return udev_device;
+        fprintf(stderr, "[udev-spoof] Spoofing parent query for virtual Sony controller. Returning marked fake parent.\n");
+        return (struct udev_device *)((uintptr_t)udev_device | 1);
+    }
+    
+    if ((uintptr_t)udev_device & 1) {
+        struct udev_device *real_dev = (struct udev_device *)((uintptr_t)udev_device & ~1);
+        if (real_get_parent) {
+            return real_get_parent(real_dev, subsystem, devtype);
+        }
+        return NULL;
     }
     
     if (real_get_parent) {
@@ -64,14 +69,29 @@ struct udev_device *udev_device_get_parent_with_subsystem_devtype(struct udev_de
     return NULL;
 }
 
+const char *udev_device_get_subsystem(struct udev_device *udev_device) {
+    init_functions();
+    
+    if ((uintptr_t)udev_device & 1) {
+        fprintf(stderr, "[udev-spoof] Spoofing subsystem query for fake parent: usb\n");
+        return "usb";
+    }
+    
+    if (real_get_subsystem) {
+        return real_get_subsystem(udev_device);
+    }
+    return NULL;
+}
+
 const char *udev_device_get_sysattr_value(struct udev_device *udev_device, const char *sysattr) {
     init_functions();
     
-    if (sysattr && is_virtual_sony_controller(udev_device)) {
-        const char *pid = real_get_property(udev_device, "ID_MODEL_ID");
+    if ((uintptr_t)udev_device & 1) {
+        struct udev_device *real_dev = (struct udev_device *)((uintptr_t)udev_device & ~1);
+        const char *pid = real_get_property(real_dev, "ID_MODEL_ID");
         int is_dualsense = pid && !strcmp(pid, "0ce6");
         
-        fprintf(stderr, "[udev-spoof] Spoofing sysattr query: %s\n", sysattr);
+        fprintf(stderr, "[udev-spoof] Spoofing sysattr query on fake parent: %s\n", sysattr);
         
         if (!strcmp(sysattr, "manufacturer")) {
             return is_dualsense ? "Sony Interactive Entertainment" : "Sony Computer Entertainment";
@@ -82,10 +102,17 @@ const char *udev_device_get_sysattr_value(struct udev_device *udev_device, const
         if (!strcmp(sysattr, "serial")) {
             return "74:e7:d6:3a:47:e8";
         }
+        if (!strcmp(sysattr, "uevent")) {
+            const char *real_uevent = real_get_sysattr ? real_get_sysattr(real_dev, sysattr) : "";
+            static char spoofed_uevent[2048];
+            snprintf(spoofed_uevent, sizeof(spoofed_uevent), "%s\nPRODUCT=054c/05c4/0100\n", real_uevent);
+            return spoofed_uevent;
+        }
     }
     
     if (real_get_sysattr) {
-        return real_get_sysattr(udev_device, sysattr);
+        struct udev_device *clean_dev = (struct udev_device *)((uintptr_t)udev_device & ~1);
+        return real_get_sysattr(clean_dev, sysattr);
     }
     return NULL;
 }
